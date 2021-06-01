cbf635f765618b94f5655bf84c6550fcc9c8e4d5
Albert J. Wong <awong.dev@gmail.com>
2021-06-01 00:22:34 -0700

    Initial creation of 500 sage book characters

---
+++ characters.js
@@ -0,0 +1,265 @@
+const characters = {
+  set1: {
+    1: {
+      chars: [
+        "山","高","的","人","個","大","上","下","在","有",
+        "手","很","隻","小","孩","家","也","我","指","五",
+      ],
+      jyutping: [
+        "saan1","gou1","dik1","jan4","go3","daai6","soeng6/soeng5","haa6","zoi6","jau5",
+        "sau2","han2","zek3","siu2","haai4","gaa1","jaa5","ngo5","zi2","ng5",
+      ],
+    },
+    2: {
+      chars: [
+        "兩","十","來","是","女","媽","爸","馬","朋","友",
+        "去","這","不","你","們","鳥","天","地","花","朵",
+      ],
+      jyutping: [
+        "loeng5","sap6","loi4","si6","neoi5","maa1","baa1","maa5","pang4","jau5",
+        "heoi3","ze5","bat1","nei5","mun4","niu5","tin1","dei6","faa1","do2/doe2",
+      ],
+    },
+    3: {
+      chars: [
+        "四","草","到","說","好","甚","麼","飛","看","裏",
+        "起","要","男","明","兔","白","書","本","會","吃",
+      ],
+      jyutping: [
+        "sei3","cou2","dou3/dou2","syut3","hou2","sam6","mo1","fei1","hon3","leoi5",
+        "hei2","jiu3","naam4","ming4","tou3","baak6","syu1","bun2","wui6/wui2/wui5","hek3",
+      ],
+    },
+    4: {
+      chars: [
+        "和","水","幾","沒","走","都","出","回","了","太",
+        "他","嗎","長","愛","喝","可","想","見","給","玩",
+      ],
+      jyutping: [
+        "wo4","seoi2","gei2","mut6","zau2","dou1","ceot1","wui4","liu5","taai3",
+        "taa1","maa3","coeng4/zoeng2","oi3","hot3","ho2","soeng2","gin3","kap1","waan2/wun6",
+      ],
+    },
+    5: {
+      chars: [
+        "就","把","樹","頭","耳","快","得","子","多","又",
+        "妹","雨","誰","門","貓","開","老","紅","邊","那",
+      ],
+      jyutping: [
+        "zau6","baa2","syu6","tau4","ji5","faai3","dak1","zi2","do1","jau6",
+        "mui6","jyu5","seoi4","mun4","maau1","hoi1","lou5","hung4","bin1","naa5",
+      ],
+    },
+  },
+  set2: {
+    1: {
+      chars: [
+        "只","住","笑","着","過","她","自","己","風","還",
+        "對","跑","真","氣","心","雲","羊","果","聽","呀",
+      ],
+      jyutping: [
+        "zi2","zyu6","siu3","zoek3","gwo3","taa1","zi6","gei2","fung1","waan4",
+        "deoi3","paau2","zan1","hei3","sam1","wan4","joeng4","gwo2","teng1/ting1","aa3",
+      ],
+    },
+    2: {
+      chars: [
+        "叫","青","像","陽","唱","歌","能","雞","跳","兒",
+        "樣","色","身","吧","聲","魚","後","弟","片","哥",
+      ],
+      jyutping: [
+        "giu3","cing1/ceng1","zoeng6","joeng4","coeng3","go1","nang4","gai1","tiu3","ji4",
+        "joeng6/joeng2","sik1","san1","baa6","seng1","jyu4/jyu2","hau6","dai6","pin3/pin2","go1",
+      ],
+    },
+    3: {
+      chars: [
+        "葉","姐","為","狗","面","搖","問","呢","中","前",
+        "進","東","河","空","做","西","用","力","從","口",
+      ],
+      jyutping: [
+        "jip6","ze1/ze4/ze2","wai4/wai6","gau2","min6/min2","jiu4","man6","ni1","zung1","cin4",
+        "zeon3","dung1","ho4","hung1","zou6","sai1","jung6","lik6","cung4","hau2",
+      ],
+    },
+    4: {
+      chars: [
+        "早","公","找","路","美","晚","星","坐","請","園",
+        "興","最","知","道","學","比","春","點","綠","國",
+      ],
+      jyutping: [
+        "zou2","gung1","zaau2","lou6","mei5","maan5","sing1","zo6/co5","cing2/ceng2","jyun4/jyun2",
+        "hing1/hing3","zeoi3","zi1","dou6","hok6","bei2","ceon1","dim2","luk6","gwok3",
+      ],
+    },
+    5: {
+      chars: [
+        "以","方","洗","亮","月","圓","黃","旗","今","哭",
+        "別","火","照","怕","師","放","光","時","生","日",
+      ],
+      jyutping: [
+        "ji5","fong1","sai2","loeng6","jyut6","jyun4","wong4","kei4","gam1","huk1",
+        "bit6","fo2","ziu3","paa3","si1","fong3","gwong1","si4","sang1/saang1","jat6",
+      ],
+    },
+  },
+  set3: {
+    1: {
+      chars: [
+        "樂","蝴","蝶","麗","蟲","輕","讓","蛙","田","游",
+        "爬","忙","黑","條","毛","成","臉","橋","木","車",
+      ],
+      jyutping: [
+        "ok6/ngok6","wu4","dip6/dip2","lai6","cung4","hing1/heng1","joeng6","waa1","tin4","jau4",
+        "paa4","mong4","hak1","tiu4","mou4","sing4","lim5","kiu4","muk6","ce1",
+      ],
+    },
+    2: {
+      chars: [
+        "牛","奶","工","農","外","帶","候","打","燕","石",
+        "船","動","喜","歡","香","更","鴨","腳","幫","屋",
+      ],
+      jyutping: [
+        "ngau4","naai5","gung1","nung4","ngoi6","daai3","hau6","daa2","jin3","sek6",
+        "syun4","dung6","hei2","fun1","hoeng1","gang1/gaang1","aap3/aap2","goek3","bong1","uk1",
+      ],
+    },
+    3: {
+      chars: [
+        "每","間","哪","尾","巴","話","娃","漂","畫","房",
+        "怎","鼠","捉","向","嘴","數","閃","海","睡","被",
+      ],
+      jyutping: [
+        "mui5","gaan1/gaan3","naa5","mei5","baa1","waa6/waa2","waa1","piu1","waak6/waa6/waa2","fong4/fong2",
+        "zam2","syu2","zuk1","hoeng3","zeoi2","sou3/sou2","sim2","hoi2","seoi6","bei6/pei5",
+      ],
+    },
+    4: {
+      chars: [
+        "拉","站","藍","正","啊","金","然","滿","才","塊",
+        "肉","跟","謝","沙","窗","親","啦","應","該","雪",
+      ],
+      jyutping: [
+        "laai1","zaam6","laam4","zing3/zeng3","aa1","gam1","jin4","mun5","coi4","faai3",
+        "juk6","gan1","ze6","saa1","coeng1","can1","laa1","jing1/jing3","goi1","syut3",
+      ],
+    },
+    5: {
+      chars: [
+        "眼","睛","現","越","常","衣","穿","如","新","冬",
+        "落","變","發","望","趕","事","洞","婆","覺","年",
+      ],
+      jyutping: [
+        "ngaan5","zing1","jin6","jyut6","soeng4","ji1","cyun1","jyu4","san1","dung1",
+        "lok6","bin3","faat3","mong6","gon2","si6","dung6","po4/po1/po2","gok3/gaau3","nin4",
+      ],
+    },
+  },
+  set4: {
+    1: {
+      chars: [
+        "姑","娘","字","寫","故","烏","鴉","傘","件","悄",
+        "經","群","飄","瓜","掉","遠","認","半","張","等",
+      ],
+      jyutping: [
+        "gu1","noeng4","zi6","se2","gu3","wu1","aa1","saan3","gin6/gin2","ciu3",
+        "ging1","kwan4","piu1","gwaa1","diu6","jyun5","jing6","bun3","zoeng1","dang2",
+      ],
+    },
+    2: {
+      chars: [
+        "伯","原","追","林","菜","壞","種","肚","病","飯",
+        "全","舞","拍","傷","機","告","訴","因","所","停",
+      ],
+      jyutping: [
+        "baak3","jyun4","zeoi1","lam4","coi3","waai6","zung2","tou5","beng6","faan6",
+        "cyun4","mou5","paak3","soeng1","gei1","gou3","sou3","jan1","so2","ting4",
+      ],
+    },
+    3: {
+      chars: [
+        "電","買","慢","教","送","旁","急","再","先","直",
+        "答","蜜","蜂","採","許","連","夜","希","搬","當",
+      ],
+      jyutping: [
+        "din6","maai5","maan6","gaau3","sung3","pong4","gap1","zoi3","sin1","zik6",
+        "daap3","mat6","fung1","coi2","heoi2","lin4","je6/je2","hei1","bun1","dong1/dong3",
+      ],
+    },
+    4: {
+      chars: [
+        "雀","枝","吹","彎","座","分","定","刀","拿","桌",
+        "咬","豬","狼","牙","流","安","哈","猴","非","桃",
+      ],
+      jyutping: [
+        "zoek3","zi1","ceoi1","waan1","zo6","fan1/fan6","ding6","dou1","naa4","coek3",
+        "ngaau5","zyu1","long4","ngaa4","lau4","on1","haa1","hau4","fei1","tou4",
+      ],
+    },
+    5: {
+      chars: [
+        "秋","冷","梨","鞋","背","包","記","行","救","次",
+        "讀","終","於","處","晴","久","蛋","母","米","物",
+      ],
+      jyutping: [
+        "cau1","laang5","lei4","haai4","bui3","baau1","gei3","hang4/haang4/hong4","gau3","ci3",
+        "duk6","zung1","jyu1","cyu5/cyu3","cing4","gau2","daan6/daan2","mou5","mai5","mat6",
+      ],
+    },
+  },
+  set5: {
+    1: {
+      chars: [
+        "校","同","運","場","班","筆","名","球","汗","螞",
+        "蟻","累","服","少","午","音","期","遊","戲","奇",
+      ],
+      jyutping: [
+        "haau6","tung4","wan6","coeng4","baan1","bat1","ming4/meng2/meng4","kau4","hon6","maa5",
+        "ngai5","leoi6","fuk6","siu2/siu3","ng5","jam1","kei4","jau4","hei3","kei4",
+      ],
+    },
+    2: {
+      chars: [
+        "怪","拾","熱","短","作","假","節","父","禮","祖",
+        "雙","掃","叔","抱","勤","感","已","相","夏","泳",
+      ],
+      jyutping: [
+        "gwaai3","sap6","jit6","dyun2","zok3","gaa3/gaa2","zit3","fu6","lai5","zou2",
+        "soeng1","sou3","suk1","pou5","kan4","gam2","ji5","soeng1/soeng2/soeng3","haa6","wing6",
+      ],
+    },
+    3: {
+      chars: [
+        "姨","表","功","燈","昨","關","難","結","甜","市",
+        "街","些","城","醫","村","清","院","潔","鬧","店",
+      ],
+      jyutping: [
+        "ji4/ji1","biu2","gung1","dang1","zok6","gwaan1","naan4","git3","tim4","si5",
+        "gaai1","se1","sing4","ji1","cyun1","cing1","jyun6/jyun2","git3","naau6","dim3",
+      ],
+    },
+    4: {
+      chars: [
+        "低","滾","合","除","左","抹","椅","右","推","勞",
+        "曬","醒","危","險","紙","離","信","重","近","各",
+      ],
+      jyutping: [
+        "dai1","gwan2","hap6","ceoi4","zo2","mut3/maat3","ji2","jau6/jau2","teoi1","lou4",
+        "saai3","sing2","ngai4","him2","zi2","lei4","seon3","cung5/zung6/cung4","gan6/kan5","gok3",
+      ],
+    },
+    5: {
+      chars: [
+        "平","餓","糖","粒","刷","痛","皮","血","踢","破",
+        "伸","忽","死","抓","極","味","藏","苦","握","歲",
+      ],
+      jyutping: [
+        "ping4/peng4","ngo6","tong4/tong2","lap1","caat3","tung3","pei4","hyut3","tek3","po3",
+        "san1","fat1","sei2","zaau2/zaa1","gik6","mei6","cong4/zong6","fu2","ak1","seoi3",
+      ],
+    },
+  },
+};
+
+console.log(JSON.stringify(characters, null, 2));
+
